520033ae9566e134772a080225c3519a26deb33f
Janis Zarzeckis <jzarzeckis@evolutiongaming.com>
2019-05-14 20:47:08 +0300

    Make the rect movable

--- src/components/MovableRectApp.tsx
+++ src/components/MovableRectApp.tsx
@@ -6,8 +6,32 @@ import "./MovableRectApp.css";
 export const MovableRectApp: React.FC<{ hide: () => void }> = ({ hide }) => {
   const [ coords, setCoords ] = useState<IPos>({ x: 100, y: 100 });
   return (<div onClick={hide} className="mvApp">
-    <div className="grayRect" style={{
-      transform: `translate(${coords.x}px, ${coords.y}px)`,
-    }} onClick={(e) => e.stopPropagation()} />
+    <div
+      className="grayRect"
+      style={{
+        transform: `translate(${coords.x}px, ${coords.y}px)`,
+      }}
+      onClick={(e) => e.stopPropagation()}
+      onMouseDown={(mDownEv) => {
+        const mDownPos: IPos = { x: mDownEv.pageX, y: mDownEv.pageY };
+        const rectStartPos: IPos = Object.assign({}, coords);
+
+        const mMove = (mMoveEv: MouseEvent) => {
+          setCoords({
+            x: rectStartPos.x + mMoveEv.pageX - mDownPos.x,
+            y: rectStartPos.y + mMoveEv.pageY - mDownPos.y,
+          });
+        };
+
+        const mUp = (mUpEv: MouseEvent) => {
+          document.removeEventListener("mousemove", mMove);
+          document.removeEventListener("mouseup", mUp);
+        };
+
+        document.addEventListener("mousemove", mMove);
+
+        document.addEventListener("mouseup", mUp);
+      }}
+    />
   </div>);
 };
