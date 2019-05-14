31cecd393da23f7613631ef79bdd6da88a93e4eb
Janis Zarzeckis <jzarzeckis@evolutiongaming.com>
2019-05-14 20:28:05 +0300

    Add the gray movable rectangle

--- src/components/MovableRectApp.tsx
+++ src/components/MovableRectApp.tsx
@@ -4,7 +4,10 @@ import { IPos } from "../types";
 import "./MovableRectApp.css";
 
 export const MovableRectApp: React.FC<{ hide: () => void }> = ({ hide }) => {
-  const [ coords, setCoords ] = useState<IPos>({ x: 0.2, y: 0.2 });
+  const [ coords, setCoords ] = useState<IPos>({ x: 100, y: 100 });
   return (<div onClick={hide} className="mvApp">
+    <div className="grayRect" style={{
+      transform: `translate(${coords.x}px, ${coords.y}px)`,
+    }} onClick={(e) => e.stopPropagation()} />
   </div>);
 };
